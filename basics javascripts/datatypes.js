698348a6e1aaf3f7357c792b945f7f0bd0b65893
anujgotnochills <itsmachead6@gmail.com>
2025-08-25 20:34:29 +0530

    datatypes/variables

---
+++ basics javascripts/datatypes.js
@@ -0,0 +1,17 @@
+let state = 2
+let username = "anuj"
+let value = true
+
+
+/*
+no = 2 power53
+bigint
+string
+boolan
+null= standalone value (object)
+undefined = undefined
+symbol
+object
+*/
+
+console.log(typeof value);
---
+++ basics javascripts/variable.js
@@ -0,0 +1,6 @@
+const accountid =753951
+let accountemail = "anuj@Gmail.com"
+accountCity = "delhi"
+let accountState
+
+console.table([accountid, accountemail, accountCity, accountState])

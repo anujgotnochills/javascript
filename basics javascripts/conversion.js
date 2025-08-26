ebb197aa8f050dda897cdbdceccceab9ed399b72
anujgotnochills <itsmachead6@gmail.com>
2025-08-26 19:10:28 +0530

    conversions

--- basics javascripts/conversion.js
+++ basics javascripts/conversion.js
@@ -1,3 +1,20 @@
-let score = "90"
-let numberconversion = Number(score)
-console.log(typeof numberconversion);
+// let score = 'anuj'
+// let numberconversion = Number(score)
+// console.log(typeof numberconversion);
+// console.log(numberconversion);
+
+/* "33" = 33
+abc33 = nan
+true = 1 false = 0
+
+*/
+// let hello = 33
+// let conversion = Boolean(hello)
+// console.log(conversion);
+
+let no = 23
+let convert = String(no)
+console.log(convert);
+console.log(typeof(convert));
+
+

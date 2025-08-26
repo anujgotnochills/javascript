f13aca57da6a4362944804dc7a8c645ca200ed23
anujgotnochills <itsmachead6@gmail.com>
2025-08-26 19:29:48 +0530

    operations

---
+++ basics javascripts/operation.js
@@ -0,0 +1,13 @@
+// let value = 3456
+// let neg = -value
+// console.log(neg);
+
+// let str1 = "anuj"
+// let str2 = " chauhan"
+// console.log(str1+str2);
+
+// console.log('2'+2);
+// console.log(2+2);
+// console.log('2'+2+2);
+// console.log(2+2+'2');
+

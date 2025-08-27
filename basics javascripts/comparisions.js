fa8ae4cc742dfaa53364d46fe221bafd5e6f823e
anujgotnochills <itsmachead6@gmail.com>
2025-08-27 20:23:55 +0530

    strings

---
+++ basics javascripts/comparisions.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+// console.log(2>"1");
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// let a = 10
+// ++a;
+// console.log(a);
+
+
---
+++ basics javascripts/strings.js
@@ -0,0 +1,21 @@
+const username = "Anuj"
+const id = 25
+console.log(`hello ${username} your id is ${id},thanks`);
+const gamename = 'valorant'
+console.log(gamename.indexOf("a"));
+console.log(gamename.toUpperCase());
+console.log(gamename.length);
+
+const strings = gamename.substring(0,4)
+console.log(strings);
+
+const anotherstrings = gamename.slice(-8,2)
+console.log(anotherstrings);
+
+const trim = "  anuj "
+console.log(trim.trim());
+
+
+
+
+
